76722943ea924a5d4cf5ac3273d7e0899cecc5f7
mdauphin <themadmax@free.fr>
2015-08-13 20:12:52 +0200

    add build.sh to build single module

---
+++ build/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit on subcommand failure or on undefined variable reference
+set -e -u
+
+#export PATH=$PATH:/home/max/CodeSourcery/Sourcery_G++_Lite/bin/
+export KOBO_SCRIPT_DIR="`dirname $0`"
+
+while test $# -gt 0; do
+	if test "$1" = "clean"; then
+		echo "Just delete all files in your build directory to clean"
+		exit 0
+	else
+		echo "Unknown argument $1, exiting"
+		exit 1
+	fi
+done
+
+i=$1
+	if echo "${SKIP:-}" | grep -q \\\<"$i"\\\> ; then
+		echo "Building of $i suppressed by \$SKIP"
+		continue
+	fi
+	$KOBO_SCRIPT_DIR/scripts/$i.sh 2>&1 | tee $i.log
+	if test "${PIPESTATUS[0]}" -ne 0 ; then
+		echo "Build failed at $i.sh, aborting"
+		exit 1
+	fi
+
+echo "Library build completed successfully"
